002ba57843dd0195dde3871d074306004ee06995
Christian Ogniew <christianogniew@gmail.com>
2023-07-04 19:51:26 -0300

    last c

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,8 +3,8 @@ import CartWidget from "../CartWidget/CartWidget";
 const NavBar = () => {
   return (
     <nav>
-      <h3>HYGGE CLOTHES</h3>
-      <div p-0 m-0>
+      <h3 class="bg-black text-white">HYGGE CLOTHES</h3>
+      <div class="p-0 m-0 justify-content-center align-items-center">
         <button class="btn btn-primary purple-btn p-1 m-1">Prendas</button>
         <button class="btn btn-primary purple-btn p-1 m-1">Accesorios</button>
         <button class="btn btn-primary purple-btn p-1 m-1">
