6eee206450a1b5c0334f869efaefb044549ec7b8
Christian Ogniew <christianogniew@gmail.com>
2023-07-16 22:19:22 -0300

    terminando proyecto

--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -1,9 +1,15 @@
 import cart from "./assets/cart.svg";
+import "./CartWidget.css";
 
 const CartWidget = () => {
   return (
     <div>
-      <img src={cart} alt="cart-widget" />0
+      <img
+        className="cart-icon white-cart-icon p-1"
+        src={cart}
+        alt="cart-widget"
+      />
+      0
     </div>
   );
 };
